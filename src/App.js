a9e483f2d4d2edf1962a657b3c8f3405bb029c22
Zach Boggs <55774849+ZachBoggs@users.noreply.github.com>
2025-04-11 04:21:44 -0700

    syncing files

---
+++ src/App.js
@@ -0,0 +1,181 @@
+import React, { useState } from 'react';
+import acmLogo from './images/clubs/acmlogo.png';
+import upeLogo from './images/clubs/UPELogo.png';
+import tpcLogo from './images/clubs/TPClogo.png';
+import cfeLogo from './images/sponsors/CFE_logo.png';
+import chicoStartLogo from './images/sponsors/chicoSTART_logo.png';
+import chicoStateEECELogo from './images/sponsors/chicostatecollegeofEECE.png';
+
+import './App.css';
+
+import { useEffect, useMemo } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+  
+function App() {
+  
+  const [init, setInit] = useState(false);
+
+  // this should be run only once per application lifetime
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      //await loadAll(engine);
+      //await loadFull(engine);
+      await loadSlim(engine);
+      //await loadBasic(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+  const options = useMemo(
+    () => ({
+      fullScreen: {
+        enable: true,
+        zIndex: -1,
+      },
+      background: {
+        color: {
+          value: "#1C1C1C",
+        },
+      },
+      
+      fpsLimit: 60,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 2,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#ffffff",
+        },
+        links: {
+          color: "#CCCCCC",
+          distance: 150,
+          enable: true,
+          opacity: 0.3,
+          width: 1,
+        },
+        move: {
+          direction: "none",
+          enable: true,
+          outModes: {
+            default: "bounce",
+          },
+          random: false,
+          speed: 1,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 120,
+        },
+        opacity: {
+          value: 0.5,
+        },
+        shape: {
+          type: "star",
+        },
+        size: {
+          value: { min: 1, max: 5 },
+        },
+      },
+      detectRetina: true,
+    }),
+    [],
+  );
+
+if (init) {
+  
+  return (
+    
+    <div className="App">
+      <title>CSU Chico Hackathon</title>
+      <Particles
+        id="tsparticles"
+        particlesLoaded={particlesLoaded}
+        options={options}
+      />
+      <div style={{ height: "20vh" }}></div> {/* Spacer */}
+      <div className="title-bar">
+        CSU Chico HACKATHON
+      </div>
+      <h1>Hosting Clubs:</h1>
+      <p>
+        <img src={upeLogo} alt='upe logo' className="club-logo"/>
+        <img src={acmLogo} alt='acm logo' className="club-logo"/>
+        <img src={tpcLogo} alt='tpc logo' className="club-logo"/>
+      </p>
+
+      <p>Thanks to our sponsors:</p>
+      <p>
+        <img src={cfeLogo} alt='cfe logo' className="club-logo"/>
+        <img src={chicoStartLogo} alt='chico start logo' className="club-logo"/>
+        <img src={chicoStateEECELogo} alt='EECE logo' className="club-logo"/>
+        <header>and Miles Benton</header>
+      </p>
+
+      <header className="App-header">
+        <div style={{ width: "70%" }}>
+          <p>April 25-26, 2025, from 2:00 PM Friday to 2:30 PM Saturday </p>
+        </div>
+
+        <div style={{ width: "70%" }}>
+          <p>
+            This event is open to all students, whether you’re a first-time participant or a seasoned hackathon veteran!
+          </p>
+        </div>
+        <div style={{ width: "70%" }}>
+          <p>
+            There will be TWO Divisions with the first place prizing being $375 for Division 1 and $200 for Division 2.
+          </p>
+        </div>
+
+        <div style={{ width: "70%" }}>
+          <p>
+            To register or learn more please go to the CatsConnect page:
+          </p>
+        </div>
+
+        <a
+          className="App-link"
+          href="https://csuchico.campuslabs.com/engage/event/11056456"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          CatsConnect
+        </a>
+        <div style={{ height: "30vh" }}></div>
+      </header>
+    </div>
+  );
+}
+}
+
+export default App;
